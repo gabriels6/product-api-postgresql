27a7d7a701f7485930d7de995ec6ef90e238387e
Gabriel Gomes <gabrielsogom@gmail.com>
2021-01-30 14:14:45 -0300

    V1.0.2 Changelog: Adding Category and Product postgresql tests

--- src/Test/test.js
+++ src/Test/test.js
@@ -6,46 +6,69 @@ client.connect();
 
 describe('Controller Functions',function(){
 
-    it('should select without error',function (done){
+    describe('Product Controller', function(){
+        it('should select without error',function (done){
 
-        const rows = CRUD.Select('product',['id','name']);
-
-        rows.then((obj) => {
-            console.log(obj);
-            done();
-        }).catch((err) => done(err));
-
-    });
-
-    it('should select and join without error', function(done){
-
-        const rows = CRUD.SelectJoin("product",["id as productId","name as productName","price as Price"],"category",["name as categoryName"],"categoryId","id").then(function(obj,err){
+            const promise = CRUD.Select('product',['id','name']);
+    
+            promise.then((obj) => {
                 console.log(obj);
                 done();
-        }).catch((err) => done(err));
+            }).catch((err) => done(err));
+    
+        });
+    
+        it('should select and join without error', function(done){
+    
+            const promise = CRUD.SelectJoin("product",["id as productId","name as productName","price as Price"],"category",["name as categoryName"],"categoryId","id").then(function(obj,err){
+                    console.log(obj);
+                    done();
+            }).catch((err) => done(err));
+        });
+    
+        it('should Update without error',function(done){
+    
+            const promise = CRUD.Update("product",["name = 'abcd'"],"ID",4);
+    
+            promise.then(function(obj){
+                console.log(obj);
+                done();
+            }).catch((err) =>{
+                done(err);
+            })
+    
+        });
+    
+        it('should Delete without error',function(done){
+    
+            const promise = CRUD.Delete("product",`id = 3`);
+    
+            promise.then(function(obj){
+                console.log(obj);
+                done();
+            }).catch((err) => done(err));
+        });
     });
 
-    it('should Update without error',function(done){
+    describe('Category Controller',function(done){
+        it('Should return a list with categories, without error',function(){
 
-        const result = CRUD.Update("product",["name = 'abcd'"],"ID",4);
+            const promise =  CRUD.Select("category",["id","name"]);
 
-        result.then(function(obj){
-            console.log(obj);
-            done();
-        }).catch((err) =>{
-            done(err);
-        })
+            promise.then((obj) =>{
+                console.log(obj);
+                done(obj);
+            }).catch((err) => done(err))
+        });
+        it('Should return a category mentioned by the id',function(){
+            const promise =  CRUD.SelectByField("category",["id","name"],"id","4");
 
+            promise.then((obj) => {
+                console.log(obj);
+                done(obj);
+            }).catch((err) => done(err));
+        });
     });
-
-    it('should Delete without error',function(done){
-
-        const result = CRUD.Delete("product",`id = 3`);
-
-        result.then(function(obj){
-            console.log(obj);
-            done();
-        }).catch((err) => done(err));
-    });
+    
 });
 
